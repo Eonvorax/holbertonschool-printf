c486ae6fefbf9806ec24789395b7185b6d8e2bd2
Yasine <yasine.bns@hotmail.com>
2023-07-24 07:31:32 -0700

    create puts and putchar

--- main.h
+++ main.h
@@ -8,25 +8,20 @@ typedef struct format
 {
 	char *type;
 	void (*function)(va_list);
-}format_t;
-
+} format_t;
+/*
 format_t tab[] = {
 	{"c", print_char},
 	{"s", print_string},
-	{"%", print_percent},
 	{'\0', NULL}
 };
-
+*/
 int _putchar(char c);
+
+void _puts(char *s);
 
 void print_char(va_list list);
 
-void print_string(va_list list);
-
-void print_percent(va_list list);
-
-void print_all(const char * const string, ...)
-
-printf("1-%d 2-%c 3-%s\n", dec, char, str);
+/*void print_all(const char * const string, ...)*/
 
 #endif
