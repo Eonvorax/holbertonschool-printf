0ab13a985e38307bfce6bfa0032d8e0c226f4963
Yasine <yasine.bns@hotmail.com>
2023-07-25 07:54:26 -0700

    updated prototypes returns types

--- main.h
+++ main.h
@@ -7,24 +7,20 @@
 typedef struct format
 {
 	char *type;
-	void (*function)(va_list);
+	int (*function)(va_list);
 } format_t;
-/*
-format_t tab[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{'\0', NULL}
-};
-*/
+
 int _putchar(char c);
 
 void _puts(char *s);
 
-void print_char(va_list list);
+int print_char(va_list list);
 
-void print_string(va_list list);
+int print_string(va_list list);
 
-void print_percent(va_list list);
+int print_percent(va_list list);
+
+int get_function(char spec, va_list list);
 
 int _printf(const char *format, ...);
 
