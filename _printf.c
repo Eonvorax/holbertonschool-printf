35db0591f81f2efd32d7880a7fe952dc912f9c22
Yasine <yasine.bns@hotmail.com>
2023-07-25 03:53:14 -0700

    Fix: betty style

--- _printf.c
+++ _printf.c
@@ -5,18 +5,18 @@
  *
  * Description: Reads the given string, printing every non-specifier character.
  * When a '%' is found, tries to match the following character with a
- * predefined specifier in conv[] and if matched, uses the corresponding pointer
+ * predefined specifier in conv[] and if matched, uses the
+ * corresponding pointer
  * to print the current argument. In case of '%%', prints '%'.
  * Stops on end character '\0'.
  * Return: 0 when successful, otherwise -1
  */
-int _printf(const char * format, ...)
+int _printf(const char *format, ...)
 {
 	unsigned int i_f = 0;
 	unsigned int i_conv = 0;
 	int count;
-	/* Array of format_t structures, to convert each defined specifier
-	 to the right function pointer */
+
 	format_t conv[] = {
 		{"c", print_char},
 		{"s", print_string},
